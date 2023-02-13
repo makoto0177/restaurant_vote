7590371f174c2eb604894ae8aaa50871588f5288
大島誠 <ooshimamakoto@mba.local>
2023-02-13 21:45:45 +0900

    Add genetate時にhelperファイルを生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,9 @@ module RestaurantVote
     # Railsの日本語化設定
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    config.generators do |g|
+      g.helper false
+    end
   end
 end
