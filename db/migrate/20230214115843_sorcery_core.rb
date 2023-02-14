f44b2bcc9bb5117b9229baa34f1cc25fd158844b
大島誠 <ooshimamakoto@mba.local>
2023-02-14 21:19:44 +0900

    Add usersテーブル作成

--- db/migrate/20230214115843_sorcery_core.rb
+++ db/migrate/20230214115843_sorcery_core.rb
@@ -1,7 +1,8 @@
 class SorceryCore < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email,            null: false, index: { unique: true }
+      t.string :name, null: false, limit: 30, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
 
