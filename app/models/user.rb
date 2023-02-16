8da0e7106fc28f5623a54bb965a6be8c766c0a1e
大島誠 <ooshimamakoto@mba.local>
2023-02-16 11:29:01 +0900

    Add ユーザー新規登録機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, 
-  validates :email, presence: true, uniqueness: true, 
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
