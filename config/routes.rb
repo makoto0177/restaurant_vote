39c506a52162f91f1aabde28ebf860220dd0d047
大島誠 <ooshimamakoto@mba.local>
2023-02-16 15:28:03 +0900

    Add ユーザーログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root 'static_pages#top'
-
   resources :users, only: %i[new create]
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
