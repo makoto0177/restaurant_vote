35d7c1e47a56fb3c1b2e775f5568ac85f371f528
大島誠 <ooshimamakoto@mba.local>
2023-02-21 22:30:02 +0900

    Add 飲食店検索ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users, only: %i[new create]
   resources :posts, only: %i[index]
-  resources :restaurants, only: %i[index]
+  resources :restaurants, only: %i[index] do
+    collection do
+      get 'search'
+    end
+  end
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
