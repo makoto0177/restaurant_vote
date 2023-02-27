463101d13a25c84ee2d6bbdfa890bcedf02184f3
大島誠 <ooshimamakoto@mba.local>
2023-02-27 10:22:40 +0900

    Add 投票結果ページグラフの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @restaurants = Restaurant.where(post_id: @post.id)
+    @chart = {'飲食店aaa'=>2, '飲食店bbb'=>1}
   end  
 
   def vote
