547042135c7452e49346dc3798a4e7e30801666c
makoto <0bv3981478w2t8p@gmail.com>
2023-03-02 19:12:14 +0900

    Add 投票作成ページに検索した飲食店を表示できるように追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :restaurants, dependent: :destroy
+  has_many :restaurants, dependent: :destroy, inverse_of: :post
+  accepts_nested_attributes_for :restaurants
 
   validates :title, presence: true
 end
