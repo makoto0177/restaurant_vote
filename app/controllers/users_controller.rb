cd1d7590cb3167d9c806ae3af10bb9cfcb942023
makoto <0bv3981478w2t8p@gmail.com>
2023-03-09 14:09:41 +0900

    Fix ログイン後の遷移先修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to posts_path
+      redirect_to search_restaurants_path
     else
       render :new
     end
