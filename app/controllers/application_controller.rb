fbd1b669fa6edf0c8af6063cf98ac6ed9ce2aede
makoto <0bv3981478w2t8p@gmail.com>
2023-03-14 11:51:54 +0900

    Fix デザインの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
   
   def not_authenticated
-    redirect_to login_path, warning: t('.defaults.message.require_login')
+    redirect_to login_path, warning: t('defaults.message.require_login')
   end
 end
