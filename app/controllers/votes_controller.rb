85029aa2184f32db5e0db85ea7b41bdf65b17216
makoto <0bv3981478w2t8p@gmail.com>
2023-03-15 22:05:49 +0900

    Fix 投票機能修正

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,15 @@
 class VotesController < ApplicationController
   def create
-    restaurant = Restaurant.find(params[:restaurant_id])
-    @vote = Vote.create(user: current_user, restaurant: restaurant)
-    @post = Post.find_by(id: restaurant.post_id)
+    @restaurant = Restaurant.find(params[:vote][:restaurant_id])
+    @vote = Vote.create(user: current_user, restaurant: @restaurant)
     if @vote.save
+      @post = Post.find(@restaurant.post_id)
       redirect_to post_path(@post), success: t('.success')
     else
-      @post = Post.find(params[:id])
+      @post = Post.find(@restaurant.post_id)
       @restaurants = Restaurant.where(post_id: @post.id)
-      render :new, status: :unprocessable_entity
+      flash.now[:error] = t('.fail')
+      render template: 'posts/vote', status: :unprocessable_entity
     end
   end
 end
