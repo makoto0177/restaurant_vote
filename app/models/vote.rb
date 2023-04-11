47abf0422a195a4339cd9fd2567c28c760bccea0
makoto <0bv3981478w2t8p@gmail.com>
2023-04-11 23:45:33 +0900

    Fix 投票の作成時のコードリファクタリング

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   has_many :vote_restaurants, dependent: :destroy
 
+  validates :restaurant_id, presence: true
   validates :user_id, uniqueness: { scope: :restaurant_id, message: "投票済みです" }
 end
